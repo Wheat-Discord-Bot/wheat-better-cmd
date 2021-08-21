e1276b6e281e6729dff9404829a43dc805a10a5e
emeralddd <tunglambinbin@gmail.com>
2021-08-21 17:47:27 +0700

    fix exports error

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const Discord = require('discord.js');
 const fs = require('fs');
-module.exports = async function wheatEmbedSend(MESSAGE,embedArray) {
+module.exports.wheatSend = async function (MESSAGE,content) {
+    try
+    {
+        await MESSAGE.channel.send(content);
+    }
+    catch(error)
+    {
+        if(error)
+        {
+            await MESSAGE.author.send("Bot bị thiếu 1 trong các quyền `SEND_MESSAGES`, `EMBED_LINKS` hoặc `ATTACH_FILES`!")
+            return;
+        }
+    }
+}
+
+module.exports.wheatEmbedSend = async function (MESSAGE,embedArray) {
     try
     {
         await MESSAGE.channel.send({embeds:embedArray});
@@ -15,7 +30,7 @@ module.exports = async function wheatEmbedSend(MESSAGE,embedArray) {
     }
 }
 
-module.exports = async function wheatEmbedAttachFilesSend(MESSAGE,embedArray,attachfilesArray) {
+module.exports.wheatEmbedAttachFilesSend = async function (MESSAGE,embedArray,attachfilesArray) {
     try
     {
         await MESSAGE.channel.send({embeds:embedArray,file:attachfilesArray});
@@ -30,14 +45,14 @@ module.exports = async function wheatEmbedAttachFilesSend(MESSAGE,embedArray,att
     }
 }
 
-module.exports = async function wheatSampleEmbedGenerate() {
+module.exports.wheatSampleEmbedGenerate = async function () {
     let embed = new Discord.MessageEmbed();
     embed.setColor('#ffd500');
     embed.setThumbnail('https://cdn.discordapp.com/avatars/786234973308715008/b5188876273d8dc038739833a2e90629.png?size=1024');
     return embed;
 }
 
-module.exports = async function wheatReadJSON(addressJSON) {
+module.exports.wheatReadJSON = async function (addressJSON) {
     let json = JSON.parse(fs.readFileSync(addressJSON, 'utf8'));
     return json;
 }
